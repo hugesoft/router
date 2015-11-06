08e1d89c8814ba63be670f2e2f5f27b76f3eff1c
hugesoft <hugesoft@126.com>
2015-11-06 10:04:59 +0800

    修改：     app/__init__.pyc 	修改：     app/main/__init__.pyc 	修改：     app/main/content.pyc 	修改：     app/main/views.py 	修改：     app/main/views.pyc 	修改：     app/main/views.py~ 	修改：     config.pyc

--- app/main/views.py
+++ app/main/views.py
@@ -50,7 +50,10 @@ def index():
 		output = re.sub( '<a href="http://www.zjol.com.cn/05zjol/sitemap/indexmap.html" title="网站地图" target="_blank">网站地图</a>+','<a href="http://www.hz66.com/2015/0930/240738.shtml" title="关于湖州在线" target="_blank">关于湖州在线</a>',output)
 		output = re.sub( '<li class="topBarRgh"><a href="http://guahao.zjol.com.cn" target="_blank" title="网上挂号">网上挂号</a></li>+','',output)
 		output = re.sub( '<li class="topBarRzh"><a href="http://zzhz.zjol.com.cn/05zzhz/card/zhkindex.html" target="_blank" title="住在杭州网">住在杭州网</a></li>+','',output)
-		
+		#改图	
+		output = re.sub( 'http://img.zjolcdn.com/pic/0/07/11/20/7112062_442059.png+','http://zt.hz66.com/mmx/img/20151106/media.png',output)
+		output = re.sub( 'http://img.zjolcdn.com/pic/0/07/11/20/7112070_823120.png+','http://zt.hz66.com/mmx/img/20151106/logo.png',output)
+
 	return output	
 	
 def getZT(url):
@@ -61,5 +64,5 @@ def getZT(url):
 	restr = re.findall(' <td class="content">([\s\S]+)<td class="content"><table border="0" align="center">',output)
 	if restr:
 		output = re.sub('content.asp+','http://zt.hz66.com/system/content.asp' ,restr[0])
-		print output
+		
 		return output
